f80363a5f45960c92072cace625d91ced1586e78
Jason Elwonger <jcelwonger@gmail.com>
2017-02-08 11:40:37 -0600

    Testing atom-beautify

--- Atom Projects/Algorithms/Chapter1/AlwaysHungry.js
+++ Atom Projects/Algorithms/Chapter1/AlwaysHungry.js
@@ -1,19 +1,19 @@
 function hungry(arr) {
-  var foodTotal = 0;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] == "food") {
-      foodTotal += 1;
+    var foodTotal = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == "food") {
+            foodTotal += 1;
+        }
     }
-  }
-  if (foodTotal == 0) {
-    console.log("I'm hungry!");
-  } else if (foodTotal > 0) {
-    while (foodTotal > 0) {
-      console.log("yummy");
-      foodTotal -= 1;
+    if (foodTotal == 0) {
+        console.log("I'm hungry!");
+    } else if (foodTotal > 0) {
+        while (foodTotal > 0) {
+            console.log("yummy");
+            foodTotal -= 1;
+        }
     }
-  }
 }
-var arr = ["food","food","garbage","food"];
-var arr2 = ["garbage","refuse","bees"];
+var arr = ["food", "food", "garbage", "food"];
+var arr2 = ["garbage", "refuse", "bees"];
 hungry(arr);
