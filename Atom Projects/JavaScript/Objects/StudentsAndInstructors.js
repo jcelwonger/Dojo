2e2be81374598f1d753dc2c59232fa3e82249d68
Jason Elwonger <jcelwonger@gmail.com>
2017-02-13 12:29:01 -0600

    completed Students and Instructors assignment with optional

--- Atom Projects/JavaScript/Objects/StudentsAndInstructors.js
+++ Atom Projects/JavaScript/Objects/StudentsAndInstructors.js
@@ -1,20 +1,60 @@
-var students = [{
-        first_name: 'Michael',
-        last_name: 'Jordan'
-    },
-    {
-        first_name: 'John',
-        last_name: 'Rosales'
-    },
-    {
-        first_name: 'Mark',
-        last_name: 'Guillen'
-    },
-    {
-        first_name: 'KB',
-        last_name: 'Tone1'
-    }
-];
-for (var student in students) {
-    console.log(students[student].first_name + ' ' + students[student].last_name)
-}
+ var students = [{
+         first_name: 'Michael',
+         last_name: 'Jordan'
+     },
+     {
+         first_name: 'John',
+         last_name: 'Rosales'
+     },
+     {
+         first_name: 'Mark',
+         last_name: 'Guillen'
+     },
+     {
+         first_name: 'KB',
+         last_name: 'Tonel'
+     }
+ ];
+ for (var student in students) {
+     console.log(students[student].first_name + ' ' + students[student].last_name)
+ }
+ var users = {
+     'Students': [{
+             first_name: 'Michael',
+             last_name: 'Jordan'
+         },
+         {
+             first_name: 'John',
+             last_name: 'Rosales'
+         },
+         {
+             first_name: 'Mark',
+             last_name: 'Guillen'
+         },
+         {
+             first_name: 'KB',
+             last_name: 'Tonel'
+         }
+     ],
+     'Instructors': [{
+             first_name: 'Michael',
+             last_name: 'Choi'
+         },
+         {
+             first_name: 'Martin',
+             last_name: 'Puryear'
+         }
+     ]
+ }
+ var keys = Object.keys(users);
+ for (var i = 0; i < keys.length; i++) {
+     var user = users[keys[i]];
+     console.log(keys[i]);
+     for (var x = 0; x < user.length; x++) {
+         var position = x + 1;
+         var firstName = user[x].first_name;
+         var lastName = user[x].last_name;
+         var nameLength = (firstName.length + lastName.length);
+         console.log(position + ' - ' + firstName + ' ' + lastName + ' - ' + nameLength);
+     }
+ }
