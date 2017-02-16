a4a82abadb04145b2d5ce389482239521aa813ec
Jason Elwonger <jcelwonger@gmail.com>
2017-02-16 07:52:22 -0600

    Re-commit of sum to one assignment

--- Atom Projects/Algorithms/Chapter2/SumToOneDigit.js
+++ Atom Projects/Algorithms/Chapter2/SumToOneDigit.js
@@ -1,14 +1,19 @@
 function sumToOne(num) {
-    var numString = num.toString();
-    var loops = numString.length;
-    var total = 0;
-    while (loops > 0) {
-        while (num > 0) {
-            total += num % 10;
-            num = Math.floor(num / 10);
-        }
-        loops -= 1;
+    var total3 = 0;
+    var total2 = 0;
+    var total1 = 0;
+    while (num > 0) {
+        total3 += num % 10;
+        num = Math.floor(num / 10);
     }
-    return total;
+    while (total3 > 0) {
+        total2 += total3 % 10;
+        total3 = Math.floor(total3 / 10);
+    }
+    while (total2 > 0) {
+        total1 += total2 % 10;
+        total2 = Math.floor(total2 / 10);
+    }
+    return total1;
 }
 console.log(sumToOne(928));
