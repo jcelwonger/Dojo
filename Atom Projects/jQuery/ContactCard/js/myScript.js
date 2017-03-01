ce55a4d79425a55e6d843027d2605e14e3a8cf17
Jason Elwonger <jcelwonger@gmail.com>
2017-03-01 14:40:11 -0600

    I fell behind

--- Atom Projects/jQuery/ContactCard/js/myScript.js
+++ Atom Projects/jQuery/ContactCard/js/myScript.js
@@ -1,12 +1,14 @@
 function attachDivHandlers() {
     $('#userInfoSidebar div').click(function(e) {
         e.stopPropagation();
-        if ($(this).children('h3').is(":visible")) {
-            $(this).children('h1,h4').show();
-            $(this).children('h3').hide();
-        } else {
-            $(this).children('h1, h4').hide();
+        if ($(this).children('h3').is(':hidden')) {
+            $(this).children('h1,h4').hide();
             $(this).children('h3').show();
+            alert('if wins');
+        } else {
+            $(this).children('h1, h4').show();
+            $(this).children('h3').hide();
+            alert('else wins');
         };
     });
 };
@@ -14,18 +16,19 @@ function attachDivHandlers() {
 $(document).ready(function() {
     attachDivHandlers();
     $('form').submit(function(e) {
-        e.preventDefault(); //Do not allow the page to reload
-
+        // Do not allow the page to reload
+        e.preventDefault();
         // Gather Variables
         var firstName = $(this).find('input[name="first_name"]').val();
         var lastName = $(this).find('input[name="last_name"]').val();
         var description = $(this).find('textarea[name="description"]').val();
         // Create a new div
         var newUserInfoBox = "<div><h1>" + firstName + " " + lastName + "</h1><h4>Click for description!</h4><h3>" + description + "</h3></div>";
-        // Populate the userInfoSidebar with the info from the form
+        // Populate the userInfoSidebar with the info from the form and attach handlers
         $('#userInfoSidebar').append(newUserInfoBox);
-        $('#userInfoSidebar div').children('h3').hide();
+        $('#userInfoSidebar div h3').hide();
         attachDivHandlers();
+        //Reset the form
         $('form')[0].reset();
 
     });
