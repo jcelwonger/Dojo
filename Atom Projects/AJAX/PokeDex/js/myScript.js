5d9b8054ea403f95c94728556536ad6b49416134
Jason Elwonger <jcelwonger@gmail.com>
2017-03-01 19:45:13 -0600

    AJAX FUN!

--- Atom Projects/AJAX/PokeDex/js/myScript.js
+++ Atom Projects/AJAX/PokeDex/js/myScript.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     for (var i = 1; i <= 151; i++) {
         var pokeURL = '<img src="http://pokeapi.co/media/img/' + i + '.png" alt="" alt-url="' + i + '"/>';
         $('#pokemon').append(pokeURL);
-    }
+    };
 });
