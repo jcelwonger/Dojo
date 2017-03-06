83ab59aafd59adbbc186b809a567d73ab84caad5
Jason Elwonger <jcelwonger@gmail.com>
2017-03-06 11:22:48 -0600

    Completed Python Fundamentals

--- Python/Fundamentals/Names.py
+++ Python/Fundamentals/Names.py
@@ -1,8 +1,30 @@
+# Part 1
 students = [
     {'first_name':  'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'},
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
+for i in range(0, 4):
+    print students[i]['first_name'], students[i]['last_name']
 
-print students[0]['first_name']
+# Part 2
+users = {
+    'Students': [
+        {'first_name':  'Michael', 'last_name': 'Jordan'},
+        {'first_name': 'John', 'last_name': 'Rosales'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ],
+    'Instructors': [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'Martin', 'last_name': 'Puryear'}
+    ]
+}
+
+for key, data in users.items():
+    print key
+    count = 0
+    for value in data:
+        count += 1
+        print count, '-', value['first_name'].upper(), value['last_name'].upper(), '-', len(value['first_name'] + value['last_name'])
