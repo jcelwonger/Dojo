ca382986c079d4774651ab84dfe4ec1fb059a522
Jason Elwonger <jcelwonger@gmail.com>
2017-03-07 10:26:56 -0600

    Defeated the stupid clock hand angles assignment

--- Algorithms/Chapter2/ClockHandAngles.js
+++ Algorithms/Chapter2/ClockHandAngles.js
@@ -1,22 +1,20 @@
 function clockHandAngles(seconds) {
-
-}
-
-function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
+    var time = seconds;
+    var hours = Math.floor(time / 3600);
+    if (hours > 24) {
+        time -= 86400;
+        hours = Math.floor(time / 3600);
     }
-    return i;
+    var minutes = Math.floor((time - (hours * 3600)) / 60);
+    var seconds = Math.floor(time % 60);
+    var secondDegree = seconds * 6;
+    var minuteDegree = Math.floor((minutes * 6) + (secondDegree * 0.017));
+    var hourDegree = Math.floor((hours * 30) + (minuteDegree * 0.0832795));
+    console.log('Hour Hand: ' + hourDegree + ' degs.');
+    console.log('Minute Hand: ' + minuteDegree + ' degs.');
+    console.log('Second Hand: ' + secondDegree + ' degs.');
+
 }
 
-function myFunction() {
-    var d = new Date();
-    var x = '';
-    var h = addZero(d.getHours());
-    var m = addZero(d.getMinutes());
-    var s = addZero(d.getSeconds());
-    x = h + ':' + m + ':' + s;
-    return x
-}
-
-console.log(myFunction());
+console.log(clockHandAngles(3600));
+console.log(clockHandAngles(119730));
