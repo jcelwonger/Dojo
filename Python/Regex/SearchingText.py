ac2045c818e4b2c4f9f67253aa40f08a5bec0cc3
Jason Elwonger <jcelwonger@gmail.com>
2017-03-14 15:52:12 -0500

    almost time to go home

--- Python/Regex/SearchingText.py
+++ Python/Regex/SearchingText.py
@@ -1,7 +1,10 @@
 import re
 
-f = open('pandp.txt', 'r')
+f = open('python\\regex\pandp.txt', 'r')
 
-strings = re.findall(r'ss', f.read())
-
+strings = re.findall(r'wife', f.read())
+total = 0
+for string in strings:
+    total += 1
+print 'The total number of times wife is written is', total, 'which is nice.'
 print strings
