55f04abe6cff7277127550c35e6ed576df76d1db
Jason Elwonger <jcelwonger@gmail.com>
2017-03-17 12:45:45 -0500

    Renaming

--- Python/hello_flask/hello.py
+++ Python/hello_flask/hello.py
@@ -1,4 +1,5 @@
-from flask import Flask  # Import Flask to allow us to create our app.
+# Import Flask to allow us to create our app.
+from flask import Flask, render_template
 
 
 # Global variable __name__ tells Flask whether or not we are running the
@@ -6,12 +7,18 @@ from flask import Flask  # Import Flask to allow us to create our app.
 app = Flask(__name__)
 
 
-# The "@" symbol designates a "decorator" which attaches the following
-@app.route('/')
 # function to the '/' route. This means that whenever we send a request to
+# the "@" symbol designates a "decorator" which attaches the following
+@app.route('/')
 # localhost:5000/ we will run the following "hello_world" function.
 def hello_world():
-    return 'Hello World!'  # Return 'Hello World!' to the response.
+    # Return 'Hello World!' to the response.
+    return render_template('index.html')
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 
 app.run(debug=True)      # Run the app in debug mode.
