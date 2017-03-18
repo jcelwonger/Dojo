32ea85124574c35dabfddec6d06e199f5cfa9eef
Jason Elwonger <jcelwonger@gmail.com>
2017-03-17 21:09:58 -0500

    FUCK PYTHON

--- Python/FlaskFundamentals/GreatNumberGame/server.py
+++ Python/FlaskFundamentals/GreatNumberGame/server.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, redirect, session, random
+from flask import Flask, render_template, url_for, request, redirect, session
+import random
 
 app = Flask(__name__)
-app.secret_key = 'SecretStuff'
+app.secret_key = 'SecretStuff'  # Session Key
 
 
-@app.route('/')
+@app.route('/')  # Default Route
 def index():
     return render_template('index.html')
 
 
-app.run(debug=True)
+app.run(debug=True)  # Run in debug mode
